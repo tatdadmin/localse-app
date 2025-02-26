a1774c81804c3cbecd3e84547ee9fbcf47041b0a
iamroare <iamroarbeast@gmail.com>
2025-02-26 15:13:27 +0530

    added static data in all languages in serviceProviderNotification

--- src/models/ServiceProviderNotifications.js
+++ src/models/ServiceProviderNotifications.js
@@ -6,6 +6,18 @@ const serviceProviderNotificationsSchema = new Schema(
   {
     subject: { type: String, required: true },               // Subject of the notification
     content: { type: String, required: true },               // Content of the notification
+    subjectHindi:{type:String,default:''},
+    contentHindi:{type:String,default:''},
+    subjectUrdu:{type:String,default:''},
+    contentUrdu:{type:String,default:''},
+    subjectMarathi:{type:String,default:''},
+    contentMarathi:{type:String,default:''},
+    subjectTamil:{type:String,default:''},
+    contentTamil:{type:String,default:''},
+    subjectTelugu:{type:String,default:''},
+    contentTelugu:{type:String,default:''},
+    subjectMalayalam:{type:String,default:''},
+    contentMalayalam:{type:String,default:''},
     service_provider_mobile_number: { type: String, required: true, maxlength: 10 },  // Mobile number of the service provider
     from: { type: String, required: true },                  // Sender of the notification
     readStatus: { type: String, enum: ['0', '1'], default: '0' },  // Read status ('0' for unread, '1' for read)
