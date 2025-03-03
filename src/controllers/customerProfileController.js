d775e81d194b7f5bd022130b7b334cf880978311
iamroare <iamroarbeast@gmail.com>
2025-03-03 16:15:17 +0530

    added some small changes in permanet_address

--- src/controllers/customerProfileController.js
+++ src/controllers/customerProfileController.js
@@ -17,15 +17,15 @@ exports.updateProfile = async (req, res) => {
       return res.status(404).json({ message: "Customer profile not found" });
     }
 
-    const { first_name, last_name, email, permanen_address, title } = req.body;
+    const { first_name, last_name, email, permanent_address, title } = req.body;
     const imageUrl = req.file ? req.file.location : customerProfile.image;
 
     customerProfile.first_name = first_name || customerProfile.first_name;
     customerProfile.last_name = last_name || customerProfile.last_name;
     customerProfile.email = email || customerProfile.email;
     customerProfile.image = imageUrl;
-    customerProfile.permanen_address =
-      permanen_address || customerProfile.permanen_address;
+    customerProfile.permanent_address =
+      permanent_address || customerProfile.permanent_address;
     customerProfile.title = title || customerProfile.title;
     customerProfile.updatedAt = new Date(
       new Date().getTime() + 5.5 * 60 * 60 * 1000
